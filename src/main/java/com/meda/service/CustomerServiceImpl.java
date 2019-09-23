b24c1e387dfd480e225562f1d5d327db04037fcf
Medasele273 <medasele273@gmail.com>
2019-09-23 16:58:30 +0200

    added Autowire to the repository and service and delete hard code which is new key word

--- src/main/java/com/meda/service/CustomerServiceImpl.java
+++ src/main/java/com/meda/service/CustomerServiceImpl.java
@@ -2,13 +2,17 @@ package com.meda.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.meda.model.Customer;
 import com.meda.repository.CustomerRepository;
-import com.meda.repository.HibernateCustomerRepositoryImpl;
 
+@Service("customerService") 
 public class CustomerServiceImpl implements  CustomerService {
 	
-	private CustomerRepository customerRepository =new HibernateCustomerRepositoryImpl();
+	@Autowired
+	private CustomerRepository customerRepository ;
   
 	@Override
 	public List<Customer> findAll(){
